1baa5ab15047d33334389656fdf56c836ffc01a7
Halimah <halimahtjoa@gmail.com>
2022-10-26 17:03:37 +0700

    commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,21 @@
+import React  from "react";
+import style from "./Home.css";
+
+const Home = () => {
+    return (
+        <>
+        <div className="d-flex flex-row justify-content-center align-items-start home" 
+            style={{Home}}>            
+            <header className="header">
+                <p className="brand">Paimoon 
+                    <br/>
+                    <b>BOOK</b> Library</p>
+                <p className="ltbrand"><b>Read and Buy Books, Here!</b></p>
+            </header>
+            <br/>
+        </div>
+        </>
+    )
+}
+
+export default Home;
