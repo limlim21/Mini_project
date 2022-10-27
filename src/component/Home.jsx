a1d88aea473213370e2784f590a699dbf9729487
Halimah <halimahtjoa@gmail.com>
2022-10-27 13:11:09 +0700

    add: homepage

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,6 @@
 import React  from "react";
 import style from "./Home.css";
+import { GiSpellBook } from "react-icons/gi";
 
 const Home = () => {
     return (
@@ -7,10 +8,11 @@ const Home = () => {
         <div className="d-flex flex-row justify-content-center align-items-start home" 
             style={{Home}}>            
             <header className="header">
-                <p className="brand">Paimoon 
-                    <br/>
+                <p className="brand">
+                    Magic<GiSpellBook/>
+                    <br />
                     <b>BOOK</b> Library</p>
-                <p className="ltbrand"><b>Read and Buy Books, Here!</b></p>
+                <p className="brandlt"><b>Read and Buy Books, Here!</b></p>
             </header>
             <br/>
         </div>
