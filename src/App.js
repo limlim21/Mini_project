bb374ed1f1625b956fb31c80f28396a9ed5e6c22
Halimah <halimahtjoa@gmail.com>
2022-10-27 19:45:14 +0700

    add: navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Home from "./component/Home"
+import Home from "./component/Home.jsx";
+import Navbar from './component/Navbar/Navbar.jsx';
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Routes>
+      <Route path='/' exact element={<Home/>}/>
+    </Routes>
   );
 }
 
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,18 +1,21 @@
 import React  from "react";
 import style from "./Home.css";
 import { GiSpellBook } from "react-icons/gi";
+import Navbar from "./Navbar/Navbar.jsx";
 
 const Home = () => {
     return (
         <>
+        <Navbar/>
+                   
         <div className="d-flex flex-row justify-content-center align-items-start home" 
-            style={{Home}}>            
+            style={{Home}}>
             <header className="header">
                 <p className="brand">
                     Magic<GiSpellBook/>
                     <br />
-                    <b>BOOK</b> Library</p>
-                <p className="brandlt"><b>Read and Buy Books, Here!</b></p>
+                    <b>BOOK</b> Store</p>
+                <p className="brandlt"><b>Search and Buy Books, Here!</b></p>
             </header>
             <br/>
         </div>
